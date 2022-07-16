68e0e07dc5a9365ab572bc05e662a2e1b5f59cd4
= <=>
2022-07-16 18:38:30 +0300

    second commit

--- main.js
+++ main.js
@@ -3,3 +3,38 @@ const prompt = require('prompt-sync')({sigint: true});
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░'
+const pathCharacter = '*'
+
+class Field {
+    constructor(field, position){
+        this.field = field
+        this.position = position 
+    }
+    
+    get position(){
+        return this.position
+    }
+    
+    set position(position){
+        this.position = position
+    }
+
+    print(){
+        for(let i = 0; i < this.field.length; i++){
+            let row = this.field[i].join('')
+            console.log(row)
+        }
+    }
+
+    static generateField() {
+
+    }
+}
+
+let alan = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ], [0,0])
+
+alan.print()
