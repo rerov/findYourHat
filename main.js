8033f759202abd85274fa4d12d0e171f9cfb58c0
= <=>
2022-07-18 21:30:07 +0300

    Game completed

--- main.js
+++ main.js
@@ -6,19 +6,12 @@ const fieldCharacter = '░'
 const pathCharacter = '*'
 
 class Field {
-    constructor(field, position){
+    constructor(field, yer){
         this.field = field
-        this.position = position 
+        this.yer = yer
+        
     }
     
-    get position(){
-        return this.position
-    }
-    
-    set position(position){
-        this.position = position
-    }
-
     print(){
         for(let i = 0; i < this.field.length; i++){
             let row = this.field[i].join('')
@@ -37,4 +30,51 @@ let alan = new Field([
     ['░', '^', '░'],
   ], [0,0])
 
-alan.print()
+/*   console.log(alan.yer)
+  alan.yer[0] += 1
+  console.log(alan.yer) */
+
+/*   console.log(alan.field)
+  alan.field[0][1] = pathCharacter
+  console.log(alan.field) */
+
+const askPrompt = require('prompt-sync')({sigint:true})
+
+let i = 0 
+
+while (i < 5) {
+    console.log(alan.print())
+    const movement = askPrompt('What is your next move // choices: up, down, left, right : ')
+    if (movement === 'down') {
+        alan.yer[0] += 1
+    }
+    else if (movement === 'up') {
+        alan.yer[0] -= 1
+    }
+    else if (movement === 'left') {
+        alan.yer[1] -= 1
+    }      
+    if (movement === 'right') {
+        alan.yer[1] += 1
+    }
+
+    if(alan.yer[0] < 0 || alan.yer[1] < 0 || alan.yer[0] > 3 || alan.yer[1] > 3){
+        console.log(alan.print())
+        console.log('You are out of boundaries. You are eliminated!')
+        break 
+    }
+    else if( alan.field[alan.yer[0]][alan.yer[1]] === hole){
+        console.log(alan.print())
+        console.log("You are inside of a hole. You are eliminated")
+        break
+    }
+    else if(alan.field[alan.yer[0]][alan.yer[1]] === hat){
+        console.log(alan.print())
+        console.log('You found your hat')
+        break
+    }
+    else {
+        alan.field[alan.yer[0]][alan.yer[1]] = pathCharacter
+        console.log(alan.print())
+    }
+}
